3aa7c6fd49c259daae0e5a99b0542740b8a31d39
VivekG11 <vivekvk2903@gmail.com>
2021-06-30 22:32:09 +0530

    [Getting a random number]

--- SnakeAndLadderSimulation/Program.cs
+++ SnakeAndLadderSimulation/Program.cs
@@ -4,11 +4,23 @@ namespace SnakeAndLadderSimulation
 {
     class Program
     {
+        public const int startPosition = 0;
         static void Main(string[] args)
         {
+            /*................................................
+             * initialising position of the player
+             * ........................................*/
+           
             Console.WriteLine("Snake and Ladder Game");
             int position = 0;
             Console.WriteLine("initiallly the player is at " +position+"th position" );
+            /*........................................
+             * creating a Random method to get the number when die is rolled
+             * ......................................*/
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            Console.WriteLine("Player Throws : "+roll);
+            Console.Read();
         }
     }
 }
