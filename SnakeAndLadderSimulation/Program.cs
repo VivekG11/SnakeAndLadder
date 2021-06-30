b1971693dd6a476497b7004ab1fb492f84343222
VivekG11 <vivekvk2903@gmail.com>
2021-06-30 22:18:07 +0530

    [Initialising postion]

---
+++ SnakeAndLadderSimulation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SnakeAndLadderSimulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Snake and Ladder Game");
+            int position = 0;
+            Console.WriteLine("initiallly the player is at " +position+"th position" );
+        }
+    }
+}
