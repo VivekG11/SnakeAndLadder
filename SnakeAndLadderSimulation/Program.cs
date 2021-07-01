d6b17b138a9b9eb1778a92bfcc912aa085736780
VivekG11 <vivekvk2903@gmail.com>
2021-07-01 15:10:26 +0530

    Creating a Two Player Game

--- SnakeAndLadderSimulation/Program.cs
+++ SnakeAndLadderSimulation/Program.cs
@@ -17,51 +17,100 @@ namespace SnakeAndLadderSimulation
             int playerPos = 0;
             int finalPos = 100;
             int rollCount = 1;
-           
+            int playerOne = 0;
+            int playerTwo = 0;
+
             Console.WriteLine("Snake and Ladder Game");
             int position = 0;
-            Console.WriteLine("initiallly the player is at " +position+"th position" );
+            Console.WriteLine("initiallly the player is at " + position + "th position");
             /*........................................
              * creating a Random method to get the number when die is rolled
              * ......................................*/
             Random random = new Random();
-            while (playerPos < finalPos)
+
+            while (playerOne < finalPos && playerTwo < finalPos)
             {
-                int roll = random.Next(1, 7);
-                // Console.WriteLine("Player Throws : "+roll);
-                // playerPos += position;
+                /* int roll = random.Next(1, 7);
+                 // Console.WriteLine("Player Throws : "+roll);
+                 // playerPos += position;
 
-                int options = random.Next(1, 4);
+                 int options = random.Next(1, 4);
+                 rollCount++;
+                 switch (options)
+                 {
+                     case noPlay:
+                         Console.WriteLine("PLayer stays in the same position");
+                         break;
+                     case ladder:
+                         Console.WriteLine("Moves ahead");
+                         playerPos = playerPos + roll;
+                         break;
+                     case snake:
+                         Console.WriteLine("Goes Behind");
+                         playerPos = (playerPos - roll) < 0 ? 0 : (playerPos - roll);
+                         break;
+                     default:
+                         break;
+
+                 }
+                 if (playerPos > finalPos)
+                 {
+                     playerPos = playerPos - roll;
+                 }
+                 Console.WriteLine("Player Position :" +playerPos);*/
+                playerOne = Game(playerOne);
+               
+                playerTwo = Game(playerTwo);
+
                 rollCount++;
-                switch (options)
-                {
-                    case noPlay:
-                        Console.WriteLine("PLayer stays in the same position");
-                        break;
-                    case ladder:
-                        Console.WriteLine("Moves ahead");
-                        playerPos = playerPos + roll;
-                        break;
-                    case snake:
-                        Console.WriteLine("Goes Behind");
-                        playerPos = (playerPos - roll) < 0 ? 0 : (playerPos - roll);
-                        break;
-                    default:
-                        break;
-
-                }
-                if (playerPos > finalPos)
-                {
-                    playerPos = playerPos - roll;
-                }
-                Console.WriteLine("Player Position :" +playerPos);
             }
             /*.....................................................................
              * On reaching final position players wins the game 
              * .................................................*/
-            Console.WriteLine("Congratulations!!!!! You Won .......");
-            Console.WriteLine("Total num of die rolls = "+rollCount);
+            if (playerOne == 100)
+            {
+                Console.WriteLine("Congratulations!!!!! player One won");
+            }
+            else
+            {
+
+                Console.WriteLine("Congratulations!!!!! Player Two Won .......");
+            }
+            Console.WriteLine("Total num of die rolls = " + rollCount);
             Console.Read();
+        }
+
+        public static int Game(int playerPos)
+        {
+            Random random = new Random();
+            int roll = random.Next(1, 7);
+            // Console.WriteLine("Player Throws : "+roll);
+            // playerPos += position;
+
+            int options = random.Next(1, 4);
+
+            switch (options)
+            {
+                case noPlay:
+                    Console.WriteLine("PLayer stays in the same position");
+                    break;
+                case ladder:
+                    Console.WriteLine("Moves ahead");
+                    playerPos = playerPos + roll;
+                    break;
+                case snake:
+                    Console.WriteLine("Goes Behind");
+                    playerPos = (playerPos - roll) < 0 ? 0 : (playerPos - roll);
+                    break;
+                default:
+                    break;
+
+            }
+            if (playerPos > 100)
+            {
+                playerPos = playerPos - roll;
+            }
+            return playerPos;
         }
     }
 }
